2e61534a97f5fa2e0fe9640b466bb96045fa7007
kruthi <sreekruthikaveri@gmail.com>
2024-07-03 14:30:44 +0530

    User profile: modified

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,9 +1,38 @@
-import { FunctionComponent } from "react";
+import React, { FunctionComponent, useState, ChangeEvent } from "react";
 import Nav from "../components/Nav";
 import Sidebar from "../components/Sidebar";
 import styles from "./UserProfile.module.css";
+import EditableInput from "./EditableInput";
 
 const UserProfile: FunctionComponent = () => {
+  const [isEditable, setIsEditable] = useState(false);
+  const [inputValues, setInputValues] = useState({
+    role: "Role",
+    name: "Full Name",
+    phoneNumber1: "Phone Number 1",
+    phoneNumber2: "Phone Number 2",
+    phoneNumber3: "Phone Number 3",
+    mail: "Mail",
+    state: "State",
+    city: "City",
+    address: "Address",
+    landlineNumber: "Landline Number"
+  });
+
+  const handleEditClick = () => {
+    setIsEditable(!isEditable);
+  };
+
+  const handleInputChange = (
+    e: ChangeEvent<HTMLInputElement>,
+    field: string
+  ) => {
+    setInputValues({
+      ...inputValues,
+      [field]: e.target.value,
+    });
+  };
+
   return (
     <div className={styles.userProfile}>
       <Nav />
@@ -12,81 +41,142 @@ const UserProfile: FunctionComponent = () => {
         <div className={styles.lastNameRow}>
           <div className={styles.userContainer}>
             <div className={styles.profileDetails}>
-              <div className={styles.header}>
-                <div className={styles.profileImage}>
-                  <div className={styles.logo} />
-                  <img
-                    className={styles.cameraIcon}
-                    loading="lazy"
-                    alt=""
-                    src="/camera.svg"
-                  />
+              <div
+                style={{ display: "flex", flexDirection: "row", gap: "12vw" }}
+              >
+                <div className={styles.header}>
+                  <div className={styles.profileImage}>
+                    <div className={styles.logo} />
+                    <button style={{
+                      border: 'none',
+                      background: 'none',
+                      cursor: 'pointer'
+                    }}>
+                      <img className={styles.cameraIcon}
+                        loading="lazy"
+                        alt=""
+                        src="/camera.svg" />
+                    </button>
+                  </div>
                 </div>
-              </div>
-              <div className={styles.editProfile}>
-                <button className={styles.editButton}>
-                  <div className={styles.editButtonChild} />
-                  <a className={styles.edit}>Edit</a>
-                  <img
-                    className={styles.materialSymbolseditIcon}
-                    alt=""
-                    src="/materialsymbolsedit.svg"
-                  />
-                </button>
+                <div className={styles.editProfile}>
+                  <button
+                    className={styles.editButton}
+                    onClick={handleEditClick}
+                  >
+                    <div className={styles.editButtonChild} />
+                    <a className={styles.edit}>
+                      {isEditable ? "Save" : "Edit"}
+                    </a>
+                    <img
+                      className={styles.materialSymbolseditIcon}
+                      alt=""
+                      src="/materialsymbolsedit.svg"
+                    />
+                  </button>
+                </div>
               </div>
             </div>
-            <div className={styles.firstNameValueWrapper}>
-              <div className={styles.firstNameValue}>
-                <div className={styles.nameDetails}>
-                  <div className={styles.firstNameInput}>
-                    <div className={styles.firstNameField}>
-                      <div className={styles.firstName}>First Name</div>
-                      <div className={styles.phoneNumberInput}>
-                        <div className={styles.profilePicture}>
-                          <div className={styles.profilePictureChild} />
-                          <div className={styles.firstName1}>First Name</div>
-                        </div>
-                        <input
-                          className={styles.phoneNumber}
-                          placeholder="Phone Number*"
-                          type="text"
-                        />
-                      </div>
-                    </div>
-                    <div className={styles.firstNameInput1}>
-                      <div className={styles.lastName}>Last Name</div>
-                      <div className={styles.lastNameContainer}>
-                        <div className={styles.phoneNumberRow}>
-                          <div className={styles.phoneNumberRowChild} />
-                          <input
-                            className={styles.lastName1}
-                            placeholder="Last Name"
-                            type="text"
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className={styles.contactNumber}>
-                    <div className={styles.contactNumberChild} />
-                    <input
-                      className={styles.phoneNo}
-                      placeholder="Phone no."
-                      type="text"
-                    />
-                  </div>
+            <div style={{display: 'flex', flexDirection: 'row', gap: '10vw'}}>
+              <div style={{display: 'flex', flexDirection: 'column',  gap: '2vw', width: '30vw'}}>
+                <div style={{display: 'grid', gridTemplateColumns: '1fr 2fr', gap: '2vw', textAlign: 'left', whiteSpace: 'nowrap', alignItems: 'center'}}>
+                  You are
+                  <EditableInput
+                    isEditable={isEditable}
+                    value={inputValues.role}
+                    onChange={(e) => handleInputChange(e, "role")}
+                  />
                 </div>
-                <div className={styles.firstNameRow}>
-                  <div className={styles.mail}>Mail</div>
-                  <div className={styles.emailInput}>
-                    <div className={styles.emailInputChild} />
-                    <input
-                      className={styles.mail1}
-                      placeholder="Mail"
-                      type="text"
-                    />
-                  </div>
+                <div style={{display: 'grid', gridTemplateColumns: '1fr 2fr', gap: '2vw', textAlign: 'left', whiteSpace: 'nowrap', alignItems: 'center'}}>
+                  Name
+                  <EditableInput
+                    isEditable={isEditable}
+                    value={inputValues.name}
+                    onChange={(e) => handleInputChange(e, "name")}
+                  />
                 </div>
+                <div style={{display: 'grid', gridTemplateColumns: '1fr 2fr', gap: '2vw', textAlign: 'left', whiteSpace: 'nowrap', alignItems: 'center'}}>
+                  Phone Number 1
+                  <EditableInput
+                    isEditable={isEditable}
+                    value={inputValues.phoneNumber1}
+                    onChange={(e) => handleInputChange(e, "phoneNumber1")}
+                  />
+                </div>
+                <div style={{display: 'grid', gridTemplateColumns: '1fr 2fr', gap: '2vw', textAlign: 'left', whiteSpace: 'nowrap', alignItems: 'center'}}>
+                  Phone Number 2
+                  <EditableInput
+                    isEditable={isEditable}
+                    value={inputValues.phoneNumber2}
+                    onChange={(e) => handleInputChange(e, "phoneNumber2")}
+                  />
+                </div>
+                <div style={{display: 'grid', gridTemplateColumns: '1fr 2fr', gap: '2vw', textAlign: 'left', whiteSpace: 'nowrap', alignItems: 'center'}}>
+                  Phone Number 3
+                  <EditableInput
+                    isEditable={isEditable}
+                    value={inputValues.phoneNumber3}
+                    onChange={(e) => handleInputChange(e, "phoneNumber3")}
+                  />
+                </div>
+              </div>
+              <div style={{display: 'flex', flexDirection: 'column', gap: '2vw', width: '30vw'}}>
+              <div style={{display: 'grid', gridTemplateColumns: '1fr 2fr', gap: '2vw', textAlign: 'left', whiteSpace: 'nowrap', alignItems: 'center'}}>
+                  Mail
+                  <EditableInput
+                    isEditable={isEditable}
+                    value={inputValues.mail}
+                    onChange={(e) => handleInputChange(e, "mail")}
+                  />
+                </div>
+                <div style={{display: 'grid', gridTemplateColumns: '1fr 2fr', gap: '2vw', textAlign: 'left', whiteSpace: 'nowrap', alignItems: 'center'}}>
+                  State
+                  <EditableInput
+                    isEditable={isEditable}
+                    value={inputValues.state}
+                    onChange={(e) => handleInputChange(e, "state")}
+                  />
+                </div>
+                <div style={{display: 'grid', gridTemplateColumns: '1fr 2fr', gap: '2vw', textAlign: 'left', whiteSpace: 'nowrap', alignItems: 'center'}}>
+                  City
+                  <EditableInput
+                    isEditable={isEditable}
+                    value={inputValues.city}
+                    onChange={(e) => handleInputChange(e, "city")}
+                  />
+                </div>
+                <div style={{display: 'grid', gridTemplateColumns: '1fr 2fr', gap: '2vw', textAlign: 'left', whiteSpace: 'nowrap', alignItems: 'center'}}>
+                  Address
+                  <EditableInput
+                    isEditable={isEditable}
+                    value={inputValues.address}
+                    onChange={(e) => handleInputChange(e, "address")}
+                  />
+                </div>
+                <div style={{display: 'grid', gridTemplateColumns: '1fr 2fr', gap: '2vw', textAlign: 'left', whiteSpace: 'nowrap', alignItems: 'center'}}>
+                  Landline Number
+                  <EditableInput
+                    isEditable={isEditable}
+                    value={inputValues.landlineNumber}
+                    onChange={(e) => handleInputChange(e, "landlineNumber")}
+                  />
+                </div>
+              </div>
+            </div>
+            <div style={{display: 'flex', flexDirection: 'column', gap: '2vh'}}>
+              <div style={{display: 'flex', flexDirection: 'column', gap: '1vw'}}>
+                Subscribe for updates from Real Estate.
+                <div style={{display: 'flex', flexDirection: 'row', gap: '1vw', marginLeft: '2vw'}}>
+                  <input type="checkbox" id="subscribe" style={{width: '18px', height: '18px', transition: '0.3s', cursor: 'pointer'}} />
+                  Other Promotional Mailers
+                </div>
+              </div>
+              <div style={{display: 'flex', flexDirection: 'row', whiteSpace: 'nowrap', gap: '0.4vw'}}>
+                By clicking below you agree to the <span style={{color: '#2697E0', textDecoration: 'underline'}}>Terms and Conditions</span>
+              </div>
+              <button style={{width: '9vw', height: '2.5vw',display: 'block', marginLeft: '30vw', borderRadius: '2vw', border: '1px transparent', backgroundImage: `linear-gradient(125.33deg, #2697e0, #784dc6)`, color: "white", fontFamily: "var(--font-montserrat)", fontSize: '16px'}}>Save Profile</button>
+              <div style={{display: 'flex', flexDirection: 'row', gap: '0.4vw', marginBottom: '3vh'}}>
+                To delete your account <span style={{color: '#2697E0', textDecoration: 'underline'}}>click here</span>
               </div>
             </div>
           </div>
