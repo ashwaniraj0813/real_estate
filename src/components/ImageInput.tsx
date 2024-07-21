9451a00ba4597739ae1ce808cd78282fb2fd5117
Sooraj Reddy <soorajreddy@Soorajs-MacBook-Air.local>
2024-07-21 16:32:28 +0530

    conflict resolved

--- src/components/ImageInput.tsx
+++ src/components/ImageInput.tsx
@@ -1,13 +1,19 @@
-import { FunctionComponent } from "react";
+import React, { FunctionComponent } from "react";
 import styles from "./ImageInput.module.css";
 
 export type ImageInputProps = {
   handleImageChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   handleRemoveImage: (index: number) => void;
   selectedImages: File[];
+  className?: string; // Add className prop if you intend to use it
 };
 
-const ImageInput: FunctionComponent<ImageInputProps> = ({ handleImageChange, handleRemoveImage, selectedImages }) => {
+const ImageInput: FunctionComponent<ImageInputProps> = ({
+  handleImageChange,
+  handleRemoveImage,
+  selectedImages,
+  className = "", // Default value for className
+}) => {
   return (
     <div className={[styles.imageInput, className].join(" ")}>
       <label htmlFor="imageInput" className={styles.uploadLabel}>
@@ -30,7 +36,7 @@ const ImageInput: FunctionComponent<ImageInputProps> = ({ handleImageChange, han
           style={{ display: "none" }}
         />
       </label>
-      
+
       <div className={styles.fileList}>
         {selectedImages.map((image, index) => (
           <div key={index} className={styles.fileItem}>
@@ -40,7 +46,7 @@ const ImageInput: FunctionComponent<ImageInputProps> = ({ handleImageChange, han
               className={styles.removeButton}
               onClick={() => handleRemoveImage(index)}
             >
-              X
+              x
             </button>
           </div>
         ))}
