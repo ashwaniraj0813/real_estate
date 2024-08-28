a23056fb3b05d61e2253c70a70ad7f1372b85b22
ashwaniraj0813 <122291624+ashwaniraj0813@users.noreply.github.com>
2024-08-28 15:05:52 +0530

    added components in Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,6 +12,22 @@ const Footer: FunctionComponent<FooterType> = ({ className = "" }) => {
   const onAboutUsTextClick = useCallback(() => {
     navigate("/aboutus");
   }, [navigate]);
+  
+  const onBuyTextClick = useCallback(() => {
+    navigate("/property-listings-page");
+  }, [navigate]);
+
+  const onRentTextClick = useCallback(() => {
+    navigate("/rent");
+  }, [navigate]);
+
+  const onSellTextClick = useCallback(() => {
+    navigate("/sell");
+  }, [navigate]);
+
+  const onAdminTextClick = useCallback(() => {
+    navigate("/admin-login");
+  }, [navigate]);
 
   return (
     <section className={[styles.footer, className].join(" ")}>
@@ -37,6 +53,9 @@ const Footer: FunctionComponent<FooterType> = ({ className = "" }) => {
           <h4>Company</h4>
           <ul>
             <li onClick={onAboutUsTextClick}>About us</li>
+            <li onClick={onBuyTextClick}>Buy</li>
+            <li onClick={onRentTextClick}>Post Property</li>
+            <li onClick={onAdminTextClick}>Admin</li>
             <li>Contact us</li>
             <li>Careers with us</li>
             <li>Terms & Conditions</li>
@@ -65,7 +84,7 @@ const Footer: FunctionComponent<FooterType> = ({ className = "" }) => {
           <h4>Contact Us</h4>
           <p>Toll-Free: 1800 41 99099</p>
           <p>9:30 AM to 6:30 PM (Mon-Sun)</p>
-          <p>Email: feedback@99acres.com</p>
+          <p>Email: feedback@BasilAbode.com</p>
         </div>
       </div>
     </section>
